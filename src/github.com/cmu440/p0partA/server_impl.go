ad8f637f4dbb4c5978ca676e7db49b6ac22a6342
adamska <2639980868@qq.com>
2023-12-02 20:25:19 +0800

    feat:use chan to add concurrency safety to active and dropped count

--- src/github.com/cmu440/p0partA/server_impl.go
+++ src/github.com/cmu440/p0partA/server_impl.go
@@ -17,16 +17,35 @@ import (
 type keyValueServer struct {
 	store kvstore.KVStore
 
-	started       bool
-	closed        bool
-	count_active  int
-	count_dropped int
+	started bool
+	closed  bool
+
+	addActive      chan bool
+	addDropped     chan bool
+	activeRequest  chan bool
+	activeResult   chan int
+	droppedRequest chan bool
+	droppedResult  chan int
+	countActive    int
+	countDropped   int
 }
 
 // New creates and returns (but does not start) a new KeyValueServer.
 func New(store kvstore.KVStore) KeyValueServer {
 	// TODO: implement this!
-	return &keyValueServer{store: store, started: false, closed: false, count_active: 0, count_dropped: 0}
+	return &keyValueServer{
+		store:          store,
+		started:        false,
+		closed:         false,
+		addActive:      make(chan bool),
+		addDropped:     make(chan bool),
+		activeRequest:  make(chan bool),
+		activeResult:   make(chan int),
+		droppedRequest: make(chan bool),
+		droppedResult:  make(chan int),
+		countActive:    0,
+		countDropped:   0,
+	}
 
 }
 
@@ -45,28 +64,54 @@ func (kvs *keyValueServer) Start(port int) error {
 	go func() {
 		for {
 			conn, err := listener.Accept()
-			kvs.count_active++
+			kvs.AddActive()
 			if err != nil {
 				log.Fatal("line50 ", err)
 			}
-			go kvs.HandleConn(conn)
-			kvs.count_dropped++
+			kvs.HandleConn(conn)
 		}
 	}()
 
+	go kvs.chanRountine()
+
 	return nil
+}
+
+func (kvs *keyValueServer) chanRountine() { // select-style channel handler for active and dropped
+	for {
+		select {
+		case <-kvs.addActive:
+			kvs.countActive++
+		case <-kvs.activeRequest:
+			kvs.activeResult <- kvs.countActive
+		case <-kvs.addDropped:
+			kvs.countDropped++
+		case <-kvs.droppedRequest:
+			kvs.droppedResult <- kvs.countDropped
+		}
+	}
 }
 
 func (kvs *keyValueServer) Close() {
 	kvs.closed = true
 }
 
+func (kvs *keyValueServer) AddActive() {
+	kvs.addActive <- true
+}
+
+func (kvs *keyValueServer) AddDropped() {
+	kvs.addDropped <- true
+}
+
 func (kvs *keyValueServer) CountActive() int {
-	return kvs.count_active
+	kvs.activeRequest <- true
+	return <-kvs.activeResult
 }
 
 func (kvs *keyValueServer) CountDropped() int {
-	return kvs.count_dropped
+	kvs.droppedRequest <- true
+	return <-kvs.droppedResult
 }
 
 // TODO: add additional methods/functions below!
@@ -81,7 +126,8 @@ func (kvs *keyValueServer) HandleConn(conn net.Conn) {
 		if err != nil {
 			conn.Close()
 			if err == io.EOF {
-				break
+				kvs.AddDropped()
+				return
 			}
 			log.Fatal("line 86 ", err)
 			return
@@ -90,27 +136,31 @@ func (kvs *keyValueServer) HandleConn(conn net.Conn) {
 		switch args[0] {
 		case "Get": // Get:[key]
 			value := kvs.store.Get(args[1])
-			fmt.Printf("Get key %v\n", args[1])
 			for i := range value {
 				_, err := writer.Write(value[i])
 				if err != nil {
-					log.Fatal("line 97 ", err)
+					log.Fatal("line 96 ", err)
 				}
 				writer.WriteByte('\n')
 			}
 			writer.Flush()
 		case "Delete": // Delete:[key]
-			// fmt.Printf("Delete key %v\n", args[1])
 			kvs.store.Delete(args[1])
 		case "Update": // Update:[key]:[oldValCleaned]:[newVal]
-			// fmt.Printf("Update %v %v %v\n", args[1], args[2], args[3])
+			fmt.Printf("Update %v %v %v\n", args[1], args[2], args[3])
 			kvs.store.Update(args[1], []byte(args[2]), []byte(args[3]))
+			kvs.Check(args[1])
 		case "Put": // Put:[key]:[val]
-			args := strings.Split(message, ":")
-			// fmt.Printf("Put %v %v\n", args[1], args[2])
 			kvs.store.Put(args[1], []byte(args[2]))
 		default:
-			log.Fatal("line 112 no command: ", args[0])
+			log.Fatal("line 110 no command: ", args[0])
 		}
+	}
+}
+
+func (kvs *keyValueServer) Check(key string) {
+	value := kvs.store.Get(key)
+	for _, v := range value {
+		fmt.Println(string(v))
 	}
 }
